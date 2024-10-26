3180f0fefa6ff2f400f36f7d3e5e2ddb378eca5b
Jacó Antonio <99668074+Jaco1030D@users.noreply.github.com>
2024-10-25 21:13:21 -0300

    Update and rename authSlice.ts to authSlice.js

--- slices/authSlice.js
+++ slices/authSlice.js
@@ -1,50 +1,24 @@
 import { createSlice, createAsyncThunk } from "https://cdn.skypack.dev/@reduxjs/toolkit";
 import authService from "../services/authService";
-import { AppDispatch, RootState } from "../strore";
-import { State, User, userLogin, userRegister } from "./types";
+
 const userData = localStorage.getItem("user");
-let user = null
+let user = null;
 
-if (userData) 
-    user = JSON.parse(userData) as User;
+if (userData) user = JSON.parse(userData);
 
-const initialState: State = {
+// Estado inicial
+const initialState = {
   user: user ? user : null,
   error: false,
   success: false,
   loading: false,
 };
 
-const createAppAsyncThunk = createAsyncThunk.withTypes<{
-  state: RootState
-  dispatch: AppDispatch
-  rejectValue: string
-}>()
-// Register a user and sign in
-export const register = createAppAsyncThunk(
-  "auth/register",
-  async (user: userRegister, thunkAPI) => {
-    const data = await authService.register(user);
+// Registrar um usuário e fazer login
+export const register = createAsyncThunk("auth/register", async (user, thunkAPI) => {
+  const data = await authService.register(user);
 
-    // Check for errors
-    if (data.errors) {
-      return thunkAPI.rejectWithValue(data.errors[0] as string);
-    }
-
-    return data;
-  }
-);
-
-// Logout a user
-export const logout = createAppAsyncThunk("auth/logout", async () => {
-  await authService.logout();
-});
-
-// Sing in a user
-export const login = createAppAsyncThunk("auth/login", async (user: userLogin, thunkAPI) => {
-  const data = await authService.login(user);
-
-  // Check for errors
+  // Verificar erros
   if (data.errors) {
     return thunkAPI.rejectWithValue(data.errors[0]);
   }
@@ -52,6 +26,24 @@ export const login = createAppAsyncThunk("auth/login", async (user: userLogin, t
   return data;
 });
 
+// Logout de um usuário
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await authService.logout();
+});
+
+// Login de um usuário
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  const data = await authService.login(user);
+
+  // Verificar erros
+  if (data.errors) {
+    return thunkAPI.rejectWithValue(data.errors[0]);
+  }
+
+  return data;
+});
+
+// Definir o slice de autenticação
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -102,10 +94,6 @@ export const authSlice = createSlice({
       });
   },
 });
-
-export type actionsTypes = {
-    payload: string
-}
 
 export const { reset } = authSlice.actions;
 export default authSlice.reducer;
