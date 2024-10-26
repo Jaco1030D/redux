62030f5c9f9bac7a8bf9e163d12d885e39ed1c8c
Jacó Antonio <99668074+Jaco1030D@users.noreply.github.com>
2024-10-25 21:14:46 -0300

    Update and rename store.ts to store.js

--- store.js
+++ store.js
@@ -8,6 +8,5 @@ export const store = configureStore({
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+// Remoção de tipos TypeScript
+export const useAppDispatch = () => useDispatch();
